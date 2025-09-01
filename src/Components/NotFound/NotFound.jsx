bf35f00efde6a22d2a53b336288352e2dfaef019
Abstractprogrammer <Abstractprogrammer@example.com>
2025-09-01 19:04:35 +0300

    third commit

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 export default function NotFound() {
   return (
-    <div>
-      
+    <div className=' flex justify-center items-center'>
+      <h1 className=' capitalize text-5xl text-[#520B05]'>this page is not found</h1>
     </div>
   )
 }
